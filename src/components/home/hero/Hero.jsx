bd4eb94f2bc329ff2b1ea7cdff42a79962d79506
Gayratjon2003 <gayratjonabdijobborov@gmail.com>
2022-11-14 16:09:30 +0500

    searchItem added

--- src/components/home/hero/Hero.jsx
+++ src/components/home/hero/Hero.jsx
@@ -1,7 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import { BiSearch } from "react-icons/bi";
+import { products } from "../../assets/data/data";
+import SearchItems from "./SearchItems";
 
 const Hero = () => {
+  const [value, setValue] = useState("");
+  const onChange = (e) => {
+    setValue(e.target.value);
+  };
+  const onSearch = (val) => {
+    console.log(val);
+  };
   return (
     <>
       <section className="hero">
@@ -17,16 +26,23 @@ const Hero = () => {
             </label>
           </h1>
           <p>
-            High-quality Design Themes for personal or commercial use contains 6k+ items in 100 categories.
+            High-quality Design Themes for personal or commercial use contains
+            6k+ items in 100 categories.
           </p>
           <div className="search">
             <span>All Categories</span>
             <hr />
-            <input type="text" placeholder="Search Products ..."/>
-          <button>
-            <BiSearch className="searchIcon heIcon"/>
-          </button>
+            <input
+              type="text"
+              placeholder="Search Products ..."
+              onChange={onChange}
+              value={value}
+            />
+            <button onClick={() => onSearch(value)}>
+              <BiSearch className="searchIcon heIcon" />
+            </button>
           </div>
+          <SearchItems product={products} value={value} onSearch={onSearch} />
           <p>Examples: Mockup, PSD, Theme Design, Image...</p>
         </div>
       </section>
