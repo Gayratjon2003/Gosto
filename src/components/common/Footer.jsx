dc0cf7de7e0258a5fd058e77ab4421b5f74dfc19
Gayratjon2003 <gayratjonabdijobborov@gmail.com>
2022-11-15 13:20:42 +0500

    footer added

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <>
+      <footer>
+        <p>
+          Copyright &copy; GOSTO all rights reserved. Povered by {" "}
+          <a href="https://www.gayratjon.uz" target="_blank">G'ayratjon Abdijobborov</a>
+        </p>
+      </footer>
+    </>
+  );
 };
 
 export default Footer;
